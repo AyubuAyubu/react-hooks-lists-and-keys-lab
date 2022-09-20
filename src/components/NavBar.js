28a225f3b0663a446947ea24fdea549f4591d87c
AyubuAyubu <ayubu6143@gmail.com>
2022-09-20 16:20:16 +0300

    Completed task

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
 import React from "react";
-
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const portfolioLinks = links.map( (link, index) => {
+     return <a key={index} href={'#' + link}>{link}</a>
+  })
+  
+  return <nav>
+     {portfolioLinks}
+     </nav>;
 }
 
 export default NavBar;
